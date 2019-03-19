e7376effe0539e8138b736c7dc34534e34ee8864
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-03-19 15:40:22 -0500

    Added several R source code files.

--- analysis/smalldemo.R
+++ analysis/smalldemo.R
@@ -9,6 +9,8 @@ K <- 13
 # SET UP ENVIRONMENT
 # ------------------
 library(readr)
+source("../code/misc.R")
+source("../code/betanmf.R")
 
 # Initialize the sequence of pseudorandom numbers.
 set.seed(1)
@@ -20,3 +22,15 @@ counts        <- suppressMessages(read_csv("../data/droplet.csv.gz"))
 class(counts) <- "data.frame"
 counts        <- as.matrix(counts)
 cat(sprintf("Loaded %d x %d counts matrix.\n",nrow(counts),ncol(counts)))
+
+# GENERATE INITIAL ESTIMATES
+# --------------------------
+
+# RUN MULTIPLICATIVE UPDATES
+# --------------------------
+cat("Fitting Poisson topic model using multiplicative updates.\n")
+# TO DO.
+
+# RUN ALTERNATING SQP METHOD
+# --------------------------
+cat("Fitting Poisson topic model using alternating SQP method.\n")
---
+++ code/betanmf.R
@@ -0,0 +1,6 @@
+# TO DO: Summarize the contents of this file.
+
+# TO DO: Explain what this function does, and how to use it.
+betanmf <- function (X) {
+
+}
