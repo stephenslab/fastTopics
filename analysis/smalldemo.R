40668d0da4af9fea6699e6f1112a5c54c6de8e16
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-03-19 15:33:01 -0500

    Added steps to smalldemo.R to load data set.

--- analysis/smalldemo.R
+++ analysis/smalldemo.R
@@ -1,3 +1,5 @@
+# TO DO: Explain here what this function does, and how to use it.
+
 # SCRIPT PARAMETERS
 # -----------------
 
@@ -6,9 +8,15 @@ K <- 13
 
 # SET UP ENVIRONMENT
 # ------------------
+library(readr)
 
 # Initialize the sequence of pseudorandom numbers.
 set.seed(1)
 
 # LOAD DATA
 # ---------
+cat("Loading count data.\n")
+counts        <- suppressMessages(read_csv("../data/droplet.csv.gz"))
+class(counts) <- "data.frame"
+counts        <- as.matrix(counts)
+cat(sprintf("Loaded %d x %d counts matrix.\n",nrow(counts),ncol(counts)))
