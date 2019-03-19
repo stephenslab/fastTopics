75f9a932b091bf5a6cbdc0fb621619087e8ed406
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-03-19 15:27:18 -0500

    Added smalldemo.R.

---
+++ analysis/smalldemo.R
@@ -0,0 +1,14 @@
+# SCRIPT PARAMETERS
+# -----------------
+
+# Number of factors (topics).
+K <- 13
+
+# SET UP ENVIRONMENT
+# ------------------
+
+# Initialize the sequence of pseudorandom numbers.
+set.seed(1)
+
+# LOAD DATA
+# ---------
