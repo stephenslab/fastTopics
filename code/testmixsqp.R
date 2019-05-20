3bad1814209704a83160adefae5ccc63b30164fc
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-05-20 16:48:00 -0500

    Made a few small adjustments to testmixsqp.R.

--- code/testmixsqp.R
+++ code/testmixsqp.R
@@ -18,26 +18,25 @@ w       <- mixdata$w
 m  <- ncol(L)
 x0 <- rep(1/m,m)
 
-# FIT MODEL USING EM
-# ------------------
-cat("Fitting model by iterating EM updates.\n")
-fit1 <- mixem(L,w,x0,numiter = 1000)
-
 # FIT MODEL USING SQP
 # -------------------
 cat("Fitting model by iterating SQP updates.\n")
-fit2 <- mixsqp(L,w,x0,numiter = 14)
+fit1 <- mixsqp(L,w,x0,numiter = 14)
+
+# FIT MODEL USING EM
+# ------------------
+cat("Fitting model by iterating EM updates.\n")
+fit2 <- mixem(L,w,x0,numiter = 1000)
 
 # SUMMARIZE RESULTS
 # -----------------
 cat(sprintf("Objective at true solution: %0.12f\n",mixdata$value))
-cat(sprintf("Objective at EM solution:   %0.12f\n",fit1$value))
-cat(sprintf("Objective at SQP solution:  %0.12f\n",fit2$value))
-cat(sprintf("Largest difference between true and EM solutions:  %0.1e\n",
-            max(abs(mixdata$x - fit1$x))))
+cat(sprintf("Objective at SQP solution:  %0.12f\n",fit1$value))
+cat(sprintf("Objective at EM solution:   %0.12f\n",fit2$value))
 cat(sprintf("Largest difference between true and SQP solutions: %0.1e\n",
+            max(abs(mixdata$x - fit1$x))))
+cat(sprintf("Largest difference between true and EM solutions: %0.1e\n",
             max(abs(mixdata$x - fit2$x))))
 cat(sprintf("EM updates yield improvement between %0.1e and %0.1e.\n",
-            min(-diff(fit1$progress$obj)),max(-diff(fit1$progress$obj))))
-cat(sprintf("SQP updates yields improvement between %0.1e and %0.1e.\n",
-            0,0))
+            min(-diff(fit2$progress$obj)),max(-diff(fit2$progress$obj))))
+
---
+++ code/testmixsqp2.R
@@ -0,0 +1,2 @@
+# This script will verify convergence of mixsqp to the correct
+# solution for a more challenging example.
