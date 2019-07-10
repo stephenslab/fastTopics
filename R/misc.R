4fa7f1c2c8aca2a3fbf22b65e8768696763e6c7a
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-07-09 22:50:23 -0500

    Implemented function verify.matrix.

--- R/misc.R
+++ R/misc.R
@@ -1,3 +1,17 @@
+# Verify that x is a minimally valid non-negative matrix.
+verify.matrix <- function (x, arg.name = deparse(substitute(x))) {
+  arg.name <- sprintf("\"%s\"",arg.name)
+  msg <- paste("Input argument",arg.name,"should be a non-negative numeric",
+               "matrix with at least two 2 and 2 columns, and all entries",
+               "should be finite and non-missing")
+  if (!((is.matrix(x) & is.numeric(x)) | inherits(x,"Matrix")))
+    stop(msg)
+  else if (!(nrow(x) > 1 & ncol(x) > 1 & all(x >= 0) &
+             all(is.finite(x)) & !any(is.na(x))))
+    stop(msg)
+  return(TRUE)
+}
+
 # Scale each column A[,i] by b[i].
 scale.cols <- function (A, b)
   t(t(A) * b)
@@ -7,7 +21,29 @@ scale.cols <- function (A, b)
 #' @export
 #' 
 loglik.poisson <- function (X, fit, e = 1e-15) {
-  return(0)
+
+  # Verify input X.
+  # TO DO.
+    
+  # Input argument "fit" should be a list with list elements "F" and
+  # "L".
+  if (!is.list(fit))
+    stop("Input argument \"fit\" should be a list")
+  if (!all(is.element(c("F","L"),names(fit))))
+    stop("Input argument \"fit\" should be a list containing named elements ",
+         "\"F\" and \"L\"")
+  F <- fit$F
+  L <- fit$L
+  
+  # Verify input F.
+  # TO DO.
+  
+  # Verify input L.
+  # TO DO.
+      
+  # Compute the Poisson log-likelihood after removing terms that do
+  # not depend on L or F.
+  return(-cost(X,tcrossprod(L,F),e))
 }
 
 # Compute the log-likelihood for the multinomial topic model. Input X
---
+++ tests/testthat/test_misc.R
@@ -0,0 +1,5 @@
+context("misc")
+
+test_that("",{
+
+})
