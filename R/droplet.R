82560524ae97747d8772545dc9fde1dc444b5709
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-09-08 12:11:18 -0500

    Compressed the droplet data, and started working on vignette.

---
+++ R/droplet.R
@@ -0,0 +1,28 @@
+#' @name droplet
+#'
+#' @title Droplet single-cell RNA-seq read count data from Montoro
+#'   \emph{et al} (2018)
+#'
+#' @docType data
+#' 
+#' @description \emph{Add details about data set here.}
+#' 
+#' @format \code{droplet} is a 7,193 x 17,133 sparse matrix.
+#' \emph{Add more details about data format here.}
+#' 
+#' @references
+#'
+#' D. T. Montoro emph{et al} (2018). A revised airway epithelial
+#' hierarchy includes CFTR-expressing ionocytes. \emph{Nature} \bold{560},
+#' 319–-324.
+#' 
+#' @keywords data
+#'
+#' @examples
+#'
+#' # Describe example here.
+#' library(Matrix)
+#' data(droplet)
+#' # Add example here.
+#' 
+NULL
