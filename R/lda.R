4933d4a146ef747892ea261bf9cbbce5e538b80d
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-11-03 22:11:35 -0600

    Added a couple items to TODO.txt; working on initial implementation of lda().

--- R/lda.R
+++ R/lda.R
@@ -1,6 +1,6 @@
 # TO DO: Explain here briefly what this function does, and how to use
 # it.
-lda <- function (X, L, F, alpha = rep(1,ncol(K)), numiter = 1000) {
+lda <- function (X, L, F, alpha = rep(1,ncol(F)), numiter = 1000) {
 
   # Get the number of rows (n) and columns (m) of X, and the number of
   # topics.
@@ -18,12 +18,15 @@ lda <- function (X, L, F, alpha = rep(1,ncol(K)), numiter = 1000) {
 
     # E STEP
     # ------
-    # Update the expected topic counts and expected word counts.
+    # Update the expected topic counts (N) and expected word counts (M).
     N <- matrix(0,n,k)
     M <- matrix(0,m,k)
     for (i in 1:n)
       for (j in 1:m) {
-        # TO DO.
+        pij   <- F[j,] * exp(digamma(L[i,]))
+        pij   <- pij / sum(pij)
+        N[i,] <- N[i,] + X[i,j]*pij
+        M[j,] <- M[j,] + X[i,j]*pij
       }
 
     # M STEP
@@ -42,4 +45,9 @@ lda <- function (X, L, F, alpha = rep(1,ncol(K)), numiter = 1000) {
   # probabilities (F), and the value of the objective at each
   # iteration ("value").
   return(list(F = F,L = L,value = value))
+}
+
+# TO DO: Explain here what this function does, and how to use it.
+elbo.lda <- function (X, F, L, alpha) {
+  # TO DO.
 }
