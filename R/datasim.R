70095070312c6a5120c4ee0b308e6a0e9960fc17
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-12-17 08:41:09 -0600

    Completed a first-draft implementation of simulate_count_data which will be used in the examples and tests; added roxygen2 docs for simulate_count_data, too.

--- R/datasim.R
+++ R/datasim.R
@@ -1,22 +1,26 @@
 #' @title Simulate count data
 #'
-#' @description Simulate an n x m matrix X such that X[i,j] is Poisson
-#' with rate (mean) Y[i,j], such that Y = tcrossprod(L,F), L is an n x
-#' k matrix, and F is an m x k matrix. The entries of matrix L are
-#' drawn uniformly at random between 0 and lmax, and the entries of
-#' matrix F are drawn uniformly at random between 0 and fmax. The
-#' return value is a list containing the data matrix X, and the
-#' factors used to simulate the data, L and F. This function is mainly
-#' used internally to simulate small data sets for testing the package
-#' functions.
+#' @description Simulate a counts matrix \code{X} such that
+#'   \code{X[i,j]} is Poisson with rate (mean) \code{Y[i,j]}, where
+#'   \code{Y = tcrossprod(L,F)}, \code{L} is an n x k loadings
+#'   ("activations") matrix, and \code{F} is an m x k factors ("basis
+#'   vectors") matrix. The entries of matrix \code{L} are drawn
+#'   uniformly at random between 0 and \code{lmax}, and the entries of
+#'   matrix \code{F} are drawn uniformly at random between 0 and
+#'   \code{fmax}.
 #'
-#' Only minimal argument checking is performed.
+#'   Note that only minimal argument checking is performed. This
+#'   function is mainly used to simulate small data sets for the examples
+#'   and package tests.
 #' 
-#' @param n Number of rows in simulated count matrix. 
+#' @param n Number of rows in simulated count matrix. The number of
+#'   rows should be at least 2.
 #'
-#' @param m Number of columns in simulated count matrix.
+#' @param m Number of columns in simulated count matrix. The number of
+#'   columns should be at least 2.
 #'
-#' @param k Number of factors used to determine Poisson rates.
+#' @param k Number of factors used to determine Poisson rates. The
+#'   number of factors should be 1 or more.
 #'
 #' @param fmax Factors are drawn uniformly at random between 0 and
 #'   \code{fmax}.
@@ -28,22 +32,46 @@
 #'   a sparse matrix in compressed, column-oriented format; see
 #'   \code{\link[Matrix]{sparseMatrix}}.
 #' 
-#' @return 
+#' @return The return value is a list containing the data matrix
+#'   \code{X} and the factorization, \code{F} and \code{L}, used to
+#'   determine the Poisson rates.
 #'
 #' @importFrom methods as
 #' @importFrom stats runif
 #' @importFrom stats rpois
+#'
+#' @export
 #' 
-simulate_count_data <- function (n, m, fmax = 1, lmax = 1, sparse = FALSE) {
+simulate_count_data <- function (n, m, k, fmax = 1, lmax = 1, sparse = FALSE) {
+
+  # Check inputs.
+  if (n < 2)
+    stop("Input argument \"n\" should be 2 or more")
+  if (m < 2)
+    stop("Input argument \"m\" should be 2 or more")
+  if (k < 1)
+    stop("Input argument \"k\" should be 1 or more")
+  if (fmax <= 0)
+    stop("Input argument \"fmax\" should be a positive number")
+  if (lmax <= 0)
+    stop("Input argument \"lmax\" should be a positive number")
+  
+  # Simulate the data.
   F <- matrix(runif(m*k,0,fmax),m,k)
   L <- matrix(runif(n*k,0,lmax),n,k)
   X <- matrix(rpois(n*m,tcrossprod(L,F)),n,m)
   if (sparse)
     X <- as(X,"dgCMatrix")
 
-  #
-  # TO DO: Add row and column names to X, F and L.
-  #
+  # Add row and column names to outputs.
+  rownames(X) <- paste0("i",1:n)
+  rownames(L) <- paste0("i",1:n)
+  colnames(X) <- paste0("j",1:m)
+  rownames(F) <- paste0("j",1:m)
+  colnames(F) <- paste0("k",1:k)
+  colnames(L) <- paste0("k",1:k)
   
+  # Return a list containing: (1) the data matrix, X; (2) the factors
+  # matrix, F; and (3) the loadings matrix, L.
   return(list(X = X,F = F,L = L))
 }
