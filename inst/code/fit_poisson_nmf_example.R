9cb59543020cda347128fb1939f17e078a72fbb3
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-02-03 16:39:41 -0600

    Fixed a couple mistakes in the plots in fit_poisson_nmf_example.R.

--- inst/code/fit_poisson_nmf_example.R
+++ inst/code/fit_poisson_nmf_example.R
@@ -10,15 +10,15 @@ fit0 <- fit_poisson_nmf(X,k = 3,numiter = 50,verbose = FALSE)
 
 # COMPARE DIFFERENT METHODS
 # -------------------------
-fit.em     <- fit_poisson_nmf(X,fit0 = fit0,numiter = 750,method = "em",verbose = FALSE)
 fit.mu     <- fit_poisson_nmf(X,fit0 = fit0,numiter = 999,method = "mu",verbose = FALSE)
+fit.em     <- fit_poisson_nmf(X,fit0 = fit0,numiter = 750,method = "em",verbose = FALSE)
 fit.ccd    <- fit_poisson_nmf(X,fit0 = fit0,numiter = 300,method = "ccd",verbose = FALSE)
 fit.scd    <- fit_poisson_nmf(X,fit0 = fit0,numiter = 200,method = "scd",verbose = FALSE)
 fit.altsqp <- fit_poisson_nmf(X,fit0 = fit0,numiter = 150,method = "altsqp",verbose = FALSE)
 
 # Plot improvement in solution over time.
-clrs <- c("skyblue","royalblue","tomato","orange","magenta")
-plot_progress_poisson_nmf(list(em = fit.em,mu = fit.mu,ccd = fit.ccd,scd = fit.scd,
+clrs <- c("royalblue","skyblue","tomato","orange","magenta")
+plot_progress_poisson_nmf(list(mu = fit.mu,em = fit.em,ccd = fit.ccd,scd = fit.scd,
                                altsqp = fit.altsqp),
                           color = clrs)
 
@@ -30,13 +30,14 @@ fit.ccd2    <- fit_poisson_nmf(Y,fit0 = fit0,numiter = 300,method = "ccd",verbos
 fit.scd2    <- fit_poisson_nmf(Y,fit0 = fit0,numiter = 200,method = "scd",verbose = FALSE)
 fit.altsqp2 <- fit_poisson_nmf(Y,fit0 = fit0,numiter = 150,method = "altsqp",verbose = FALSE)
 
-plot_progress_poisson_nmf(list(em = fit.em,mu = fit.mu,ccd = fit.ccd,scd = fit.scd,
-                               altsqp = fit.altsqp,em.sp = fit.em2,ccd.sp = fit.ccd2,
-                               scd.sp = fit.scd2,altsqp.sp = fit.altsqp2),
+clrs <- clrs[-1]
+plot_progress_poisson_nmf(list(em = fit.em,ccd = fit.ccd,scd = fit.scd,altsqp = fit.altsqp,
+                               em.sp = fit.em2,ccd.sp = fit.ccd2,scd.sp = fit.scd2,
+                               altsqp.sp = fit.altsqp2),
                           add.point.every = Inf,
                           color = rep(clrs,times = 2),
-                          linetype = rep(c("solid","twodash"),each = 5),
-                          linesize = rep(0.5,10))
+                          linetype = rep(c("solid","twodash"),each = 4),
+                          linesize = rep(0.5,9))
 
 # COMPARE WITH VS. WITHOUT EXTRAPOLATION
 # --------------------------------------
