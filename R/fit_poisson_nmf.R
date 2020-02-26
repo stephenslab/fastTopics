03b57546f1fc45da02590847c73b959d15b62259
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-02-26 11:37:15 -0600

    Removed stray character in R code.

--- R/fit_poisson_nmf.R
+++ R/fit_poisson_nmf.R
@@ -522,7 +522,7 @@ fit_poisson_nmf_main_loop <- function (X, fit, numiter, method, control,
   fit$progress <- progress
   return(fit)
 }
-<
+
 # This implements a single (non-extrapolated) update of the factors
 # and loadings. The output is the updated "fit". Note that, because no
 # extrapolation scheme is used, the extrapolated and non-extrapolated
