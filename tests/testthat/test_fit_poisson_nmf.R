ae65f33733d7f34d0d5467e122be15e940ae02c1
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-03-04 14:29:07 -0600

    Small change to one of the tests.

--- tests/testthat/test_fit_poisson_nmf.R
+++ tests/testthat/test_fit_poisson_nmf.R
@@ -374,11 +374,11 @@ test_that(paste("Extrapolated updates achieve solutions that are as good",
   out <- generate_test_data(80,100,3)
   X   <- out$X
 
-  # Get a good initial estimate by running 100 iterations of the CCD
+  # Get a good initial estimate by running 100 iterations of the EM
   # algorithm.
   capture.output(
     fit0 <- fit_poisson_nmf(X,fit0 = init_poisson_nmf(X,F = out$F,L = out$L),
-                            numiter = 100,method = "ccd"))
+                            numiter = 100,method = "em"))
 
   # Run 100 non-extrapolated updates using each of the methods.
   numiter <- 100
