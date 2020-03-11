8d8526894425a071064adeb145c9e47f2e0a2ac8
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-03-11 12:41:55 -0500

    Implemented scd.R demo script.

--- inst/code/scd.R
+++ inst/code/scd.R
@@ -2,30 +2,38 @@
 
 # SIMULATE DATA
 # -------------
-set.seed(1)
-n <- 10
+set.seed(49)
+n <- 20
 w <- rpois(n,2)
 a <- abs(rnorm(n))
 b <- abs(rnorm(n))
 
-stop()
-
 # Solve the optimization problem
 #
-#   minimize f(x) = sum(x*b - w*log(a + b*b))
+#   minimize f(x) = sum(b*x - w*log(y))
+#   subject to y = a + b*x,
+#              x >= 0.
 # 
-# using a simple sequential quadratic programming method.
-numiter <- 100
+# using a simple sequential quadratic programming (SQP) method.
+numiter <- 20
 f       <- rep(0,numiter)
 x       <- 1
 for (i in 1:numiter) {
 
   # Compute the value of the objective at x.
-  y <- a + b*x;
-  f <- sum(b*x - w*log(a + b*x))
+  y    <- a + b*x;
+  f[i] <- sum(b*x - w*log(y))
     
   # Compute the gradient and Hessian at x.
-  u <- l/y
+  u <- b/y
   h <- sum(w*u^2)
-  g <-  - sum(w*u) - u + h*x;
+  g <- sum(b - w*u)
+
+  # Update x.
+  x <- max(0,x - g/h)
 }
+
+# Plot the improvement in the solution over time.
+plot(1:numiter,f - min(f) + 1e-15,type = "b",pch = 20,col = "darkblue",
+     log = "y",xlab = "iteration",ylab = "distance to min")
+
