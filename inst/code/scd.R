8dc1ddecedc788e6fb634b6345d907b35a65fa2c
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-03-11 12:45:31 -0500

    Made a few small improvements to the scd.R script.

--- inst/code/scd.R
+++ inst/code/scd.R
@@ -1,4 +1,6 @@
-# TO DO: Explain here what this script does, and how to use it.
+# A small script to illustrate the co-ordinate ascent updates in the
+# sequential co-ordinate descent (SCD) algorithm described by Lin &
+# Boutros (2018).
 
 # SIMULATE DATA
 # -------------
@@ -8,16 +10,16 @@ w <- rpois(n,2)
 a <- abs(rnorm(n))
 b <- abs(rnorm(n))
 
-# Solve the optimization problem
+# Solve the following 1-d optimization problem:
 #
 #   minimize f(x) = sum(b*x - w*log(y))
 #   subject to y = a + b*x,
 #              x >= 0.
 # 
 # using a simple sequential quadratic programming (SQP) method.
+x       <- 1
 numiter <- 20
 f       <- rep(0,numiter)
-x       <- 1
 for (i in 1:numiter) {
 
   # Compute the value of the objective at x.
@@ -32,8 +34,9 @@ for (i in 1:numiter) {
   # Update x.
   x <- max(0,x - g/h)
 }
+cat(sprintf("solution: %0.6f\n",x))
 
 # Plot the improvement in the solution over time.
 plot(1:numiter,f - min(f) + 1e-15,type = "b",pch = 20,col = "darkblue",
-     log = "y",xlab = "iteration",ylab = "distance to min")
+     log = "y",xlab = "iteration",ylab = "distance to minimum")
 
