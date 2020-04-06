85aa649d62027e5936f1e2f421bba3ae29aaaae6
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-04-06 12:00:54 -0500

    Added tests for fit_poisson_nmf update.factors and update.loadings arguments, when method = 'scd'.

--- tests/testthat/test_fit_poisson_nmf.R
+++ tests/testthat/test_fit_poisson_nmf.R
@@ -499,4 +499,43 @@ test_that("Fixed factors and loadings to not change (aside from rescaling)",{
   expect_equal(compare_loadings_ignoring_rescaling(fit0,fit4,i0),0,
                scale = 1,tolerance = 1e-15)
 
+  # Run all variants of the SCD algorithm.
+  capture.output(
+    fit1 <- fit_poisson_nmf(X,fit0 = fit0,numiter = numiter,
+                            update.factors = j,update.loadings = i,
+                            method = "scd",control = list(nc = 1)))
+  capture.output(
+    fit2 <- fit_poisson_nmf(X,fit0 = fit0,numiter = numiter,
+                            update.factors = j,update.loadings = i,
+                            method = "scd",control = list(nc = 4)))
+  capture.output(
+    fit3 <- fit_poisson_nmf(Y,fit0 = fit0,numiter = numiter,method = "scd",
+                            update.factors = j,update.loadings = i,
+                            control = list(nc = 1)))
+  capture.output(
+    fit4 <- fit_poisson_nmf(Y,fit0 = fit0,numiter = numiter,method = "scd",
+                            update.factors = j,update.loadings = i,
+                            control = list(nc = 4)))
+
+  # The factors that are not selected for updating should not change,
+  # aside from a rescaling.
+  expect_equal(compare_factors_ignoring_rescaling(fit0,fit1,j0),0,
+               scale = 1,tolerance = 1e-15)
+  expect_equal(compare_factors_ignoring_rescaling(fit0,fit2,j0),0,
+               scale = 1,tolerance = 1e-15)
+  expect_equal(compare_factors_ignoring_rescaling(fit0,fit3,j0),0,
+               scale = 1,tolerance = 1e-15)
+  expect_equal(compare_factors_ignoring_rescaling(fit0,fit4,j0),0,
+               scale = 1,tolerance = 1e-15)
+
+  # The loadings that are not selected for updating should not change,
+  # aside from a rescaling.
+  expect_equal(compare_loadings_ignoring_rescaling(fit0,fit1,i0),0,
+               scale = 1,tolerance = 1e-15)
+  expect_equal(compare_loadings_ignoring_rescaling(fit0,fit2,i0),0,
+               scale = 1,tolerance = 1e-15)
+  expect_equal(compare_loadings_ignoring_rescaling(fit0,fit3,i0),0,
+               scale = 1,tolerance = 1e-15)
+  expect_equal(compare_loadings_ignoring_rescaling(fit0,fit4,i0),0,
+               scale = 1,tolerance = 1e-15)
 })
