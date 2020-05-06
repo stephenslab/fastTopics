7165b1fad9d1a912382e6817b7efc9de9e935b5f
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-05-06 07:31:30 -0500

    Added comments describing fit_binom_topic_model.

--- R/binom.R
+++ R/binom.R
@@ -1,6 +1,8 @@
-# TO DO: Explain here what this function does, and describe the inputs
-# and outputs.
-#
+# Fit model parameters p0,p1 of binomial model n1 ~ Binom(n,p), where
+# the binomial success rates are p = q*p1 + (1-q)*p0, and n = n0 + n1.
+# Input argument "e" is a small positive number added to the
+# likelihood and gradient to avoid NaNs; specifically, logarithms of
+# zero and division by zero.
 #'
 #' @importFrom stats optim
 #' 
@@ -18,7 +20,7 @@ fit_binom_topic_model <- function (n0, n1, q, e = 1e-5) {
   # where x = c(p0,p1).
   f <- function (x) {
     p <- pbinom(x[1],x[2],q)
-    return(-(sum(n1*log(p + e) + n0*log(1 - p + e))))
+    return(-(sum(n1*log(p+e) + n0*log(1-p+e))))
   }
 
   # Define function for computing the gradient of the negative
@@ -26,15 +28,14 @@ fit_binom_topic_model <- function (n0, n1, q, e = 1e-5) {
   g <- function (x) {
     p <- pbinom(x[1],x[2],q)
     g <- n1/(p+e) - n0/(1-p+e)
-    return(c(-sum((1-q)*g),
-             -sum(q*g)))
+    return(c(-sum(g*(1-q)),
+             -sum(g*q)))
   }
   
   # Fit the binomial probabilities using the "limited-memory"
   # quasi-Newton method implemented in "optim".
-  out <- optim(c(0.5,0.5),f,g,method = "L-BFGS-B",
-           lower = c(0,0),upper = c(1,1),
-           control = list(factr = 1e-8,maxit = 100))
+  out <- optim(c(0.5,0.5),f,g,method = "L-BFGS-B",lower = c(0,0),
+               upper = c(1,1),control = list(factr = 1e-8,maxit = 100))
 
   # Output MLEs of p0 and p1, and the other "optim" outputs.
   names(out$par) <- c("p0","p1")
