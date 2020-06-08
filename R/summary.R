370349d3ee938a7f1932b715c172162e464ae3b3
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-06-07 23:00:03 -0500

    Added more summary functionality.

--- R/summary.R
+++ R/summary.R
@@ -1,20 +1,39 @@
-#' @title Summarize Poisson NMF Fit
+#' @title Summarize Poisson NMF or Multinomial Topic Model Fit
 #'
-#' @description Add description of function here.
+#' @description \code{summary} method for the \dQuote{poisson_nmf_fit}
+#'   and \dQuote{multinom_topic_model_fit} classes.
 #'
-#' @param object Describe input argument "object" here.
+#' @param object An object of class \dQuote{poisson_nmf_fit} or
+#'   \dQuote{multinom_topic_model_fit}. The former is usually the result
+#'   of calling \code{\link{fit_poisson_nmf}}; the latter is usually the
+#'   result of calling \code{link{poisson2multinom}}.
 #'
 #' @param \dots Additional arguments passed to the generic method.
 #'
 #' @method summary poisson_nmf_fit
 #'
-#' @return The function \code{summary.poisson_nmf_fit} computes and
-#'   returns a list of statistics summarizing the model fit.
+#' @return The functions \code{summary.poisson_nmf_fit} and
+#'   \code{summary.multinom_topic_model_fit} compute and return a list
+#'   of statistics summarizing the model fit.
 #' 
 #' @export
 #' 
 summary.poisson_nmf_fit <- function (object, ...) {
+  out <- summary(poisson2multinom(object))
+  class(out) <- c("summary.poisson_nmf_fit","list")
+  return(out)
+}
+
+#' @rdname summary.poisson_nmf_fit
+#'
+#' @method summary multinom_topic_model_fit
+#' 
+#' @export
+#' 
+summary.multinom_topic_model_fit <- function (object, ...) {
+  #
   # TO DO.
+  #  
   class(out) <- c("summary.poisson_nmf_fit","list")
   return(out)
 }
@@ -29,7 +48,19 @@ summary.poisson_nmf_fit <- function (object, ...) {
 #' @export
 #' 
 print.summary.poisson_nmf_fit <- function (x, ...) {
-  # TO DO.
+  print.summary.multinom_topic_model_fit(x,...)
   return(invisible(x))
 }
 
+#' @rdname summary.poisson_nmf_fit
+#'  
+#' @method print summary.multinom_topic_model_fit
+#' 
+#' @export
+#'
+print.summary.multinom_topic_model_fit <- function (x, ...) {
+  #
+  # TO DO.
+  #  
+  return(invisible(x))
+}
