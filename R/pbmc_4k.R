46719078fe91abadf5581339a71f40c4ca35a53b
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-12-01 13:53:30 -0600

    Added help page for pbmc_4k data.

---
+++ R/pbmc_4k.R
@@ -0,0 +1,52 @@
+#' @name pbmc_4k
+#'
+#' @title Mixture of FACS-purified PBMC Single-Cell RNA-seq data
+#'
+#' @docType data
+#' 
+#' @description These data are a selection of the reference
+#'   transcriptome profiles generated via single-cell RNA sequencing
+#'   (RNA-seq) of 10 bead-enriched subpopulations, described in Zheng
+#'   \emph{et al} (2017). The data are unique molecular identifier (UMI)
+#'   counts for 21,952 genes in 3,774 cells. Since the majority of the
+#'   UMI counts are zero, they are efficiently stored as a 3,774 x
+#'   21,952 sparse matrix. These data are used in the vignette
+#'   illustrating how fastTopics can be used to analyze to single-cell
+#'   RNA-seq data.
+#'
+#' @format \code{pbmc_4k} is a list with the following elements:
+#' 
+#' \describe{
+#'
+#'   \item{counts}{3,774 x 21,952 sparse matrix of UMI counts, with
+#'      rows corresponding to samples (cells) and columns corresponding to
+#'      genes. It is an object of class \code{"dgCMatrix"}).}
+#' 
+#'   \item{samples}{Data frame containing information about the
+#'     samples, including cell barcode and source FACS population
+#'     ("celltype").}
+#' 
+#'   \item{genes}{Data frame containing information and the genes,
+#'     including gene symbol and Ensembl identifier.}
+#'
+#'   \item{fit}{Poisson non-negative matrix factorization (NMF) fitted
+#'     to the UMI count data, with rank \code{k = 6}. See the vignette
+#'     how the Poisson NMF model fitting was performed.}
+#' }
+#'
+#' @references
+#' G. X. Y. Zheng \emph{et al} (2017). Massively parallel digital
+#' transcriptional profiling of single cells. \emph{Nature Communications}
+#' \bold{8}, 14049. \url{https://doi.org/10.1038/ncomms14049}
+#' 
+#' @keywords data
+#'
+#' @examples
+#' library(Matrix)
+#' data(pbmc_4k)
+#' cat(sprintf("Number of cells: %d\n",nrow(pbmc_4k$counts)))
+#' cat(sprintf("Number of genes: %d\n",ncol(pbmc_4k$counts)))
+#' cat(sprintf("Proportion of counts that are non-zero: %0.1f%%.\n",
+#'             100*mean(pbmc_4k$counts > 0)))
+#' 
+NULL
