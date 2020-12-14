c4ee44b3674541bed2031c35d24498d3a7356faa
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-12-14 14:38:52 -0600

    Added version requirement for ggrepel.

--- R/fit_poisson_nmf.R
+++ R/fit_poisson_nmf.R
@@ -409,7 +409,7 @@ fit_poisson_nmf <- function (X, k, fit0, numiter = 100,
       method.text <- "CCD"
     cat(sprintf("Running %d %s updates, %s extrapolation ",numiter,
         method.text,ifelse(control$extrapolate,"with","without")))
-    cat("(fastTopics 0.3-191).\n")
+    cat("(fastTopics 0.3-192).\n")
   }
   
   # INITIALIZE ESTIMATES
