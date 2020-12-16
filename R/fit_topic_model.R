ce2415bca69ef6db04cf29324753bb4038abe044
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-12-16 15:19:21 -0600

    Completed a first-draft implementation of fit_topic_model_simple.

--- R/fit_topic_model.R
+++ R/fit_topic_model.R
@@ -14,12 +14,24 @@
 #'
 #' @param k The number of topics. Must be 2 or greater.
 #'
+#' @param numiter.main
+#'
+#' @param numiter.refine
+#'
+#' @param method.main
+#'
+#' @param method.refine
+#'
 #' @param init.method The method used to initialize the factors and
 #'   loadings. See \code{\link{init_poisson_nmf}} for details.
 #'
 #' @param control.init A list of parameters controlling the behaviour
 #'   of the optimization and Topic SCORE method in the call to
 #'   \code{init_poisson_nmf}.
+#'
+#' @param control.main
+#'
+#' @param control.refine
 #'
 #' @param verbose When \code{verbose = TRUE}, information about the
 #'   progress of the model fitting is printed to the console. See
@@ -36,14 +48,18 @@
 #' library(Matrix)
 #' set.seed(1)
 #' X <- simulate_count_data(80,100,k = 3,sparse = TRUE)$X
+#' fit <- fit_topic_model_simple(X,k = 3)
+#' print(summary(fit))
 #' 
 #' @export
 #'
-fit_topic_model_simple <- function (X, k,
-                                    init.method = c("topicscore","random"),
-                                    control.init = list(),
-                                    verbose = TRUE) {
-
+fit_topic_model_simple <-
+  function (X, k, numiter.main = 100, numiter.refine = 100, method.main = "em",
+            method.refine = "scd", init.method = c("topicscore","random"),
+            control.init = list(), control.main = list(numiter = 4),
+            control.refine = list(numiter = 4,extrapolate  = TRUE),
+            verbose = TRUE) {
+      
   # Check inputs.
   if (!((is.numeric(X) & is.matrix(X)) | is.sparse.matrix(X)))
     stop("Input argument \"X\" should be a numeric matrix (a \"matrix\" or ",
@@ -59,13 +75,24 @@ fit_topic_model_simple <- function (X, k,
   }
 
   # Initialize the Poisson NMF model fit.
-  fit0 <- init_poisson_nmf(X,k = k,init.method = init.method,
-                           control = control.init,
+  fit <- init_poisson_nmf(X,k = k,init.method = init.method,
+                          control = control.init,
+                          verbose = verbose)
+
+  # Perform the main model fitting step.
+  fit <- fit_poisson_nmf(X,fit0 = fit,numiter = numiter.main,
+                         method = method.main,control = control.main,
+                         verbose = verbose)
+  
+  # Perform the model refinement step.
+  if (numiter.refine > 0) {
+    if (verbose)
+      cat("Refining model fit.\n")
+    fit <- fit_poisson_nmf(X,fit0 = fit,numiter = numiter.refine,
+                           method = method.refine,control = control.refine,
                            verbose = verbose)
-
-  # Temporary.
-  fit <- fit0
-
+  }
+  
   # Output the multinomial topic model fit.
   return(poisson2multinom(fit))
 }
