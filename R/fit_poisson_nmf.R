e51475c4b5b2a48aa9f046f13097ffdc226b4fce
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-12-26 21:39:35 -0600

    Added back fit_topic_model output to pbmc_4k data set.

--- R/fit_poisson_nmf.R
+++ R/fit_poisson_nmf.R
@@ -409,7 +409,7 @@ fit_poisson_nmf <- function (X, k, fit0, numiter = 100,
       method.text <- "CCD"
     cat(sprintf("Running %d %s updates, %s extrapolation ",numiter,
         method.text,ifelse(control$extrapolate,"with","without")))
-    cat("(fastTopics 0.4-10).\n")
+    cat("(fastTopics 0.4-11).\n")
   }
   
   # INITIALIZE ESTIMATES
