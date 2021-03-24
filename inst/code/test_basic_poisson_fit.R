6e00a7cc7da37abb07b83bd7e4a4287035e892d9
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-03-24 16:49:01 -0500

    Added calculation of s.e. to test_basic_poisson_fit.R.

--- inst/code/test_basic_poisson_fit.R
+++ inst/code/test_basic_poisson_fit.R
@@ -1,5 +1,4 @@
 # TO DO: Explain here what this script is for, and how to use it.
-library(pracma)
 
 # Simulate binomial data, x ~ binom(s*p0).
 set.seed(1)
@@ -18,10 +17,22 @@ fit <- glm(x ~ f0 - 1,family = poisson(link = "identity"),
 f0 <- sum(x)/sum(s)
 
 # The glm estimate should be the same as f0.
-print(coef(fit) - f0)
+cat(coef(fit),f0,"\n")
 
 # Compute the s.e. of log(f0) in the basic Poisson model.
-# TO DO.
+se <- 1/sqrt(f0*sum(s))
 
 # Compute the s.e. of log(f0) using numerical integration.
-# TO DO.
+ns <- 1000
+t  <- seq(-5,-1,length.out = ns)
+w  <- rep(0,ns)
+for (i in 1:ns) {
+  w[i] <- sum(dpois(x,s*exp(t[i]),log = TRUE))
+}
+w <- exp(w - max(w))
+w <- w/sum(w)
+mu <- sum(w*t)
+se_mc <- sqrt(sum(w*(t - mu)^2))
+
+# The two s.e. calculations should be nearly the same.
+cat(se,se_mc,"\n")
