bc4646ea68cd574dec13c524f964c84d62eaa900
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-04-01 16:16:36 -0500

    Implemented gradient calculations in check_poisson_hessian.R script.

---
+++ inst/code/check_poisson_hessian.R
@@ -0,0 +1,40 @@
+# TO DO: Explain here what this script is for, and how to use it.
+library(pracma)
+
+# Simulate data x ~ Pois(u), with u = l0*f0 + l1*f1.
+set.seed(1)
+n  <- 40
+f0 <- 0.1
+f1 <- 1
+s  <- sample(10,n,replace = TRUE)
+u  <- runif(n)
+l0 <- s*(1-u)
+l1 <- s*u
+x  <- rpois(n,l0*f0 + l1*f1)
+
+# Compute the log-likelihood under the model x ~ Pois(u), with
+# Poisson rates u = l0*f0 + l1*f1.
+loglik <- function (x, l0, l1, f0, f1)
+  sum(dpois(x,l0*f0 + l1*f1,log = TRUE))
+
+# Compute the gradient of the log-likelihood with respect to log(f0)
+# and log(f1).
+loglik_grad <- function (x, l0, l1, f0, f1) {
+  u <- l0*f0 + l1*f1
+  y <- x/u - 1
+  return(c(f0*sum(l0*y),
+           f1*sum(l1*y)))
+}
+
+# Compute the second-order partial derivatives of the log-likelihood.
+loglik_hessian <- function (x, l0, l1, f0, f1) {
+
+}
+
+# Compare loglik_grad against numerical gradients calculated using
+# finite differences.
+f0 <- runif(1)
+f1 <- runif(1)
+print(grad(function (v) loglik(x,l0,l1,exp(v[1]),exp(v[2])),log(c(f0,f1))),
+      digits = 12)
+print(loglik_grad(x,l0,l1,f0,f1),digits = 12)
--- inst/code/test_basic_poisson_fit.R
+++ inst/code/test_basic_poisson_fit.R
@@ -1,5 +1,3 @@
-# TO DO: Explain here what this script is for, and how to use it.
-
 # Simulate binomial data, x ~ binom(s*p0).
 set.seed(1)
 n  <- 50
@@ -13,26 +11,25 @@ fit <- glm(x ~ f0 - 1,family = poisson(link = "identity"),
            data = data.frame(x = x,f0 = s),start = 0.5,
            control = glm.control(epsilon = 1e-10, maxit = 100))
 
-# Compute the MLE of p0 in the basic Poisson model.
+# Compute the MLE of f0.
 f0 <- sum(x)/sum(s)
 
 # The glm estimate should be the same as f0.
 cat(coef(fit),f0,"\n")
 
-# Compute the s.e. of log(f0) in the basic Poisson model.
+# Compute the s.e. of log(f0) using the Laplace approximation.
 se <- 1/sqrt(f0*sum(s))
 
 # Compute the s.e. of log(f0) using numerical integration.
 ns <- 1000
-t  <- seq(-5,-1,length.out = ns)
+t  <- seq(-6,0,length.out = ns)
 w  <- rep(0,ns)
-for (i in 1:ns) {
+for (i in 1:ns)
   w[i] <- sum(dpois(x,s*exp(t[i]),log = TRUE))
-}
-w <- exp(w - max(w))
-w <- w/sum(w)
+w  <- exp(w - max(w))
+w  <- w/sum(w)
 mu <- sum(w*t)
 se_mc <- sqrt(sum(w*(t - mu)^2))
 
-# The two s.e. calculations should be nearly the same.
+# The two s.e. calculations should be pretty close.
 cat(se,se_mc,"\n")
