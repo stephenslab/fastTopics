00e2cbb33a98f2bf15e3b57a882f85567f313922
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-04-12 16:42:50 -0500

    Implemented fit_poisson_models.

--- R/poisson.R
+++ R/poisson.R
@@ -20,3 +20,43 @@ fit_poisson_glm <-
                               data = dat,start = f,control = control))
   return(list(fit = fit,coef = summary.glm(fit)$coefficients[,"Estimate"]))
 }
+
+# For each column of the counts matrix, compute maximum-likelihood
+# estimates (MLEs) of the parameters in the single-count Poisson
+# model. Input argument "method" may be set to "glm" or any valid
+# setting for "method" in update_factors_poisson_nmf.
+#
+#' @importFrom stats glm.control
+fit_poisson_models <- function (X, L, s = rep(1,nrow(X)), method, e = 1e-15,
+                                numiter = 100, tol = 1e-8, nc = 1) {
+
+  # Get the number of topics (k) and the number of data columns (m).
+  m <- ncol(X)
+  k <- ncol(L)
+  
+  # Enure that none of the topic proportions are exactly zero or
+  # exactly one.
+  L <- pmax(L,e)
+  L <- pmin(L,1-e)
+
+  # For each column of X, compute MLEs of the Poisson model parameters.
+  if (method == "glm") {
+    control <- glm.control(epsilon = tol,maxit = numiter)
+    F <- fit_poisson_models_glm(X,L,s,control)
+  } else {
+    control <- list(numiter = numiter,nc = nc,eps = e)
+    F <- matrix(0.5,m,k)
+    F <- update_factors_poisson_nmf(X,F,s*L,1:m,method,control)
+  }
+  return(F)
+}
+
+# Implements fit_poisson_models for method = "glm".
+fit_poisson_models_glm <- function (X, L, s, control)  {
+  m <- ncol(X)
+  k <- ncol(L)
+  F <- matrix(0,m,k)
+  for (i in 1:m)
+    F[i,] <- fit_poisson_glm(X[,i],s*L,control = control)$coef
+  return(F)
+}
--- inst/code/poisson_demo.R
+++ inst/code/poisson_demo.R
@@ -13,50 +13,18 @@ s   <- 10^runif(n,-1,1)
 dat <- simulate_poisson_gene_data(n,m,k,s)
 X   <- dat$X
 L   <- dat$L
+Y   <- as(X,"dgCMatrix")
 
-# Fit a Poisson model to each combination of gene j and topic k.
-Y  <- as(X,"dgCMatrix")
-t1 <- system.time(out.em <- fit_univar_poisson_models(X,L,s,method="em"))
-t2 <- system.time(out.em2 <- fit_univar_poisson_models(X,L,s,method="em-rcpp"))
-t3 <- system.time(out.em3 <- fit_univar_poisson_models(Y,L,s,method="em-rcpp"))
-t4 <- system.time(out.optim <- fit_univar_poisson_models(X,L,s,method="optim"))
-print(t1)
-print(t2)
-print(t3)
-print(t4)
+# Fit a Poisson model for each gene.
+F1 <- fit_poisson_models(X,L,s,method = "glm")
+F2 <- fit_poisson_models(X,L,s,method = "scd",nc = 4)
+print(range(F1 - F2))
 
-# Check that the R and C++ implementations of the EM algorithm produce
-# the same, or nearly the same, likelihoods and estimates of the model
-# parameters (f0, f1).
-print(max(abs(out.em$F0 - out.em2$F0)))
-print(max(abs(out.em$F0 - out.em3$F0)))
-print(max(abs(out.em$F1 - out.em2$F1)))
-print(max(abs(out.em$F1 - out.em3$F1)))
-print(max(abs(out.em$loglik - out.em2$loglik)))
-print(max(abs(out.em$loglik - out.em3$loglik)))
-
-# Check that EM and optim produce the same, or nearly the same,
-# estimates of the model parameters, f0 and f1.
+# Compare the estimates against the Poisson rates used to simulate the
+# data.
 e <- 1e-3
-plot(out.optim$F1 + e,out.em$F1 + e,pch = 20,log = "xy")
-abline(a = 0,b = 1,col = "dodgerblue",lty = "dotted")
-
-# Compare the EM and optim log-likelihoods.
-print(range((out.em$loglik - out.optim$loglik)/out.optim$loglik))
-
-# For the selected topic, compare f1 estimates against the Poisson
-# rates used to simulate the data.
 i <- 1
-plot(dat$F[,i] + e,out.em$F1[,i] + e,pch = 20,log = "xy",
-     xlab = "true f1",ylab = "estimated f1")
-abline(a = 0,b = 1,col = "dodgerblue",lty = "dotted")
-
-# For the selected topic, compare f0 estimates against the Poisson
-# rates used to simulate the data. Since there is no "f0" used to
-# simulate the data, here we approximate f0 by taking the average of
-# the Poisson rates across all topics other than topic i.
-plot(rowMeans(dat$F[,-i]) + e,out.em$F0[,i] + e,pch = 20,log = "xy",
-     xlab = "true f0 (approx)",ylab = "estimated f0")
+plot(dat$F + e,F1 + e,pch = 20,log = "xy",xlab = "true F",ylab = "estimated F")
 abline(a = 0,b = 1,col = "dodgerblue",lty = "dotted")
 
 # Compute the log-fold change and Z-score for each gene j and topic k.
