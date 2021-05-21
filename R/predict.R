51ee00405271db35af365570f7a5aed4467b4e90
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-05-21 14:31:45 -0500

    Filled out roxygen2 docs for predict S3 methods.

--- R/predict.R
+++ R/predict.R
@@ -1,16 +1,22 @@
 #' @rdname predict
 #'
-#' @title Title Goes Here
+#' @title Predict Methods for Poisson NMF and Multinomial Topic Model
 #'
-#' @description Description goes here.
+#' @description Predict loadings based on previously fit Poisson NMF,
+#'   or predict topic proportions based on previously fit multinomial
+#'   topic model. This can be thought of as projecting data points onto
+#'   a previously estimated set of factors \code{fit$F}.
 #'
-#' @param object Describe input argument "object" here.
+#' @param object An object of class \dQuote{poisson_nmf_fit} or
+#'   \dQuote{multinom_topic_model_fit}.
+#' 
+#' @param newdata An optional counts matrix. If omitted, the loadings
+#'   estimated in the original data are returned.
 #'
-#' @param newdata Describe (optional) input "newdata" here.
+#' @param numiter The number of updates to perform.
 #'
-#' @param numiter Describe input "numiter" here.
-#'
-#' @param \dots Describe ... here.
+#' @param \dots Additional arguments passed to
+#'   \code{\link{fit_poisson_nmf}}.
 #'
 #' @seealso \code{\link{fit_poisson_nmf}}
 #' 
@@ -45,7 +51,7 @@
 #'
 #' # Simulate a 175 x 1,200 counts matrix.
 #' set.seed(1)
-#' dat <- simulate_multinom_gene_data(175,1200,3)
+#' dat <- simulate_multinom_gene_data(175,1200,k = 3)
 #'
 #' # Split the data into training and test sets.
 #' train <- dat$X[1:100,]
@@ -81,7 +87,7 @@ predict.poisson_nmf_fit <- function (object, newdata, numiter = 20, ...) {
   if (missing(newdata))
     return(object$L)
   else
-    return(project_poisson_nmf(object,newdata,numiter,...)$L)
+    return(project_poisson_nmf(newdata,object$F,numiter,...)$L)
 }
 
 #' @rdname predict
@@ -92,21 +98,31 @@ predict.poisson_nmf_fit <- function (object, newdata, numiter = 20, ...) {
 #'
 #' @export
 #' 
-predict.multinom_topic_model_fit <- function (object, newdata, numiter = 20,
-                                              ...) {
+predict.multinom_topic_model_fit <- function (object, newdata,
+                                              numiter = 20, ...) {
   if (missing(newdata))
     return(object$L)
   else
-    return(poisson2multinom(project_poisson_nmf(multinom2poisson(object),
-                                                newdata,numiter,...))$L)
+    return(poisson2multinom(project_poisson_nmf(newdata,
+                                                multinom2poisson(object)$F,
+                                                numiter,...))$L)
 }
 
-# TO DO: Explain here what this function does, and how to use it.
-project_poisson_nmf <- function (fit0, X, numiter, ...) {
-  n   <- nrow(X)
-  k   <- ncol(fit0$F)
+# Fit a Poisson non-negative factorization to X in which the factors
+# matrix is fixed to F.
+project_poisson_nmf <- function (X, F, numiter, ...) {
+
+  # Verify that fit0 and X are compatible.
+  n <- nrow(X)
+  k <- ncol(F)
+  verify.count.matrix(X)
+  if (nrow(F) != ncol(X))
+    stop("fit$F and X do not match; the number of rows of fit$F should ",
+         "equal the number of columns of X")
+
+  # Fit a Poisson NMF to the data, X, with F fixed.
   L0  <- matrix(1/k,n,k)
-  fit <- init_poisson_nmf(X,F = fit0$F,L = L0)
+  fit <- init_poisson_nmf(X,F = F,L = L0)
   fit <- fit_poisson_nmf(X,fit0 = fit,numiter = numiter,...)
   return(fit)
 }
