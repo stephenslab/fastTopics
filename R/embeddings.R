9be77339aa37980d45143b9685e990753b83686a
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-06-03 14:21:51 -0500

    Revised tsne_from_topics and the accompanying roxygen2 docs.

--- R/embeddings.R
+++ R/embeddings.R
@@ -1,30 +1,50 @@
 #' @rdname embeddings_from_topics
 #'
 #' @title Low-dimensional Embeddings from Poisson NMF or Multinomial Topic Model
-#'
-#' @description Describe pca_from_topics here.
-#'
-#' @details Add details here.
-#'
-#' @param fit Describe input argument "fit" here.
-#'
-#' @param dims Describe input argument "dims" here.
-#'
-#' @param center Describe input argument "center" here.
-#'
-#' @param scale. Describe input argument "scale." here.
+#' @description Lightweight interface for rapidly producing
+#'   low-dimensional embeddings from matrix factorizations or
+#'   multinomial topic models. The defaults used are more suitable for
+#'   producing embeddings from matrix factorizations or topic models.
 #' 
-#' @param \dots Describe additional inputs here.
+#' @details Note that since \code{tsne_plot} uses a \emph{nonlinear}
+#'   transformation of the data, distances between points are less
+#'   interpretable than a linear transformation obtained by, say, PCA.
+#'
+#' @param fit An object of class \dQuote{poisson_nmf_fit} or
+#'   \dQuote{multinom_topic_model_fit}.
+#'
+#' @param dims The number of dimensions in the embedding. In
+#'   \code{tsne_from_topics}, this is passed as argument \dQuote{dims}
+#'   to \code{\link[Rtsne]{Rtsne}}.
+#'
+#' @param center A logical value indicating whether columns of
+#'   \code{fit$L} should be zero-centered before performing PCA; passed
+#'   as argument \dQuote{center} to \code{\link[stats]{prcomp}}.
+#'
+#' @param scale. A logical value indicating whether columns of
+#'   \code{fit$L} should be scaled to have unit variance prior to
+#'   performing PCA; passed as argument \dQuote{scale.} to
+#'   \code{\link[stats]{prcomp}}.
+#' 
+#' @param \dots Additional arguments passed to
+#'   \code{\link[stats]{prcomp}} or \code{\link[Rtsne]{Rtsne}}.
+#'
+#' @return An n x d matrix containing the embedding, where n is the
+#'   number of rows of \code{fit$L}, and \code{d = dims}.
 #' 
 #' @seealso \code{\link{pca_plot}}, \code{\link{tsne_plot}},
-#'   \code{\link{umap_plot}}
+#'   \code{\link{umap_plot}}, \code{\link[stats]{prcomp}},
+#'   \code{\link[Rtsne]{Rtsne}}, \code{\link[uwot]{umap}}
 #'
+#' @examples
+#' # Add examples here.
+#' 
 #' @importFrom stats prcomp
 #' 
 #' @export
 #' 
-pca_from_topics <- function (fit, dims = 2, center = TRUE, scale. = FALSE,
-                             ...) {
+pca_from_topics <- function (fit, dims = 2, center = TRUE,
+                             scale. = FALSE, ...) {
   if (!(inherits(fit,"poisson_nmf_fit") |
         inherits(fit,"multinom_topic_model_fit")))
     stop("Input \"fit\" should be an object of class \"poisson_nmf_fit\" or ",
@@ -34,54 +54,6 @@ pca_from_topics <- function (fit, dims = 2, center = TRUE, scale. = FALSE,
 }
 
 #' @rdname embeddings_from_topics
-#' 
-umap_from_topics <- function (fit, dims, ...) {
-
-}
-
-#'
-#' #' @title t-SNE from Poisson NMF or Multinomial Topic Model
-#'
-#' @description Computes a low-dimensional nonlinear embededding of
-#'   the data from the estimated loadings or mixture proportions using
-#'   the t-SNE nonlinear dimensionality reduction method.
-#'
-#' @details This is a lightweight interface for rapidly producing
-#' t-SNE embeddings from matrix factorizations or multinomial topic
-#' models; in particular, \code{tsne_from_topics} replaces the t-SNE
-#' defaults with settings that are more suitable for visualizing the
-#' structure of a matrix factorization or topic model (e.g., the PCA
-#' step in \code{Rtsne} is activated by default, but disabled in
-#' \code{tsne_from_topics}). See Kobak and Berens (2019) for guidance
-#' on choosing t-SNE settings such as the "perplexity" and learning
-#' rate (\code{eta}).
-#'
-#' Note that since \code{tsne_plot} uses a \emph{nonlinear}
-#' transformation of the data, distances between points are less
-#' interpretable than a linear transformation visualized using
-#' \code{pca_plot} for example.
-#'
-#' @param fit An object of class \dQuote{poisson_nmf_fit} or
-#' \dQuote{multinom_topic_model_fit}.
-#'
-#' @param dims The number of dimensions in the t-SNE embedding; passed
-#'   as argument \dQuote{dims} to \code{\link[Rtsne]{Rtsne}}.
-#'
-#' @param n The maximum number of rows in the loadings matrix
-#'   \code{fit$L} to use; when the loadings matrix has more than
-#'   \code{n} rows, the t-SNE embedding is computed on a random
-#'   selection of \code{n} rows. An upper limit on the number of rows is
-#'   used because the runtime of \code{\link[Rtsne]{Rtsne}} increases
-#'   rapidly with the number of rows in the input matrix.
-#'
-#' @param scaling A numeric vector of length equal to the number of
-#'   topics specifying a scaling of the columns of \code{fit$L}; this
-#'   re-scaling is performed prior to running t-SNE. The vector should
-#'   contain non-negative numbers only. A larger value will increase the
-#'   importance, or \dQuote{weight}, of the respective topic in
-#'   computing the embedding. When \code{scaling} is \code{NULL}, no
-#'   re-scaling is performed. Note that this scaling will have no effect
-#'   if \code{normalize = TRUE}.
 #' 
 #' @param pca Whether to perform a PCA processing step in t-SNE;
 #'   passed as argument \dQuote{pca} to \code{\link[Rtsne]{Rtsne}}.
@@ -112,28 +84,16 @@ umap_from_topics <- function (fit, dims, ...) {
 #'   printed; passed as argument \dQuote{verbose} to
 #'   \code{\link[Rtsne]{Rtsne}}.
 #' 
-#' @param \dots Additional arguments passed to \code{\link[Rtsne]{Rtsne}}.
-#'
-#' @return A list with two list elements: \code{Y}, an n x d matrix
-#'   containing the embedding \code{Y} returned by
-#'   \code{\link[Rtsne]{Rtsne}}, where n is the number of rows of the
-#'   loadings matrix, and \code{d = dims}; \code{rows}, the rows of the
-#'   loadings matrix included in the t-SNE embedding.
-#'
 #' @references
-#'
 #' Kobak, D. and Berens, P. (2019). The art of using t-SNE for
 #' single-cell transcriptomics. \emph{Nature Communications} \bold{10},
 #' 5416. \url{https://doi.org/10.1038/s41467-019-13056-x}
-#' 
-#' @seealso \code{\link[Rtsne]{Rtsne}}
 #' 
 #' @importFrom Rtsne Rtsne
 #' 
 #' @export
 #' 
-tsne_from_topics <- function (fit, dims = 2, n = 5000, scaling = NULL,
-                              pca = FALSE, normalize = FALSE,
+tsne_from_topics <- function (fit, dims = 2, pca = FALSE, normalize = FALSE,
                               perplexity = 100, theta = 0.1, max_iter = 1000,
                               eta = 200, check_duplicates = FALSE,
                               verbose = TRUE, ...) {
@@ -143,41 +103,37 @@ tsne_from_topics <- function (fit, dims = 2, n = 5000, scaling = NULL,
         inherits(fit,"multinom_topic_model_fit")))
     stop("Input \"fit\" should be an object of class \"poisson_nmf_fit\" or ",
          "\"multinom_topic_model_fit\"")
-
-  # Randomly subsample, if necessary.
-  n0 <- nrow(fit$L)
-  if (n < n0)
-    rows <- sample(n0,n)
-  else
-    rows <- 1:n0
-
-  # Prepare the data for t-SNE. If requested, re-scale the columns of
-  # the loadings matrix.
-  L <- fit$L[rows,]
-  if (!is.null(scaling))
-    L <- scale.cols(L,scaling)
-
+  n <- nrow(fit$L)
+  if (n < 4)
+    stop("Rtsne cannot run on a matrix with fewer than 4 rows")
+      
   # Adjust the perplexity if it is too large for the number of samples.
-  n  <- nrow(L)
-  p0 <- floor((n - 1)/3) - 1
+  p0 <- pmax(1,floor((n - 1)/3) - 1)
   if (perplexity > p0) {
     message(sprintf(paste("Perplexity automatically changed to %d because",
-                          "original setting of %d was too large for the",
+                          "the original setting of %d was too large for the",
                           "number of samples (%d)"),p0,perplexity,n))
     perplexity <- p0
   }
   
   # Compute the t-SNE embedding.
-  out <- Rtsne(L,dims,pca = pca,normalize = normalize,perplexity = perplexity,
-               theta = theta,max_iter = max_iter,eta = eta,
-               check_duplicates = check_duplicates,
-               verbose = verbose,...)
+  out <- Rtsne(fit$L,dims,pca = pca,normalize = normalize,eta = eta,
+               perplexity = perplexity,theta = theta,max_iter = max_iter,
+               check_duplicates = check_duplicates,verbose = verbose,...)
 
-  # Return the t-SNE embedding stored as an n x dims matrix (Y), and
-  # the rows of L included in the embedding (rows).
-  Y           <- out$Y
-  rownames(Y) <- rownames(L)
+  # Return the t-SNE embedding.
+  Y <- out$Y
+  rownames(Y) <- rownames(fit$L)
   colnames(Y) <- paste0("d",1:dims)
-  return(list(Y = Y,rows = rows))
+  return(Y)
 }
 
+#' @rdname embeddings_from_topics
+#'
+#' @importFrom uwot umap
+#' 
+#' @export
+#' 
+umap_from_topics <- function (fit, dims, ...) {
+
+}
