b84f04f7ab6575eb1d621211beee71d1039f0b31
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-06-03 17:08:09 -0500

    Implemented umap_from_topics and filled out accompanying roxygen2 docs.

--- R/embeddings.R
+++ R/embeddings.R
@@ -6,16 +6,19 @@
 #'   multinomial topic models. The defaults used are more suitable for
 #'   producing embeddings from matrix factorizations or topic models.
 #' 
-#' @details Note that since \code{tsne_plot} uses a \emph{nonlinear}
-#'   transformation of the data, distances between points are less
-#'   interpretable than a linear transformation obtained by, say, PCA.
+#' @details Note that since \code{tsne_from_topics} and
+#'   \code{umap_from_topics} use nonlinear transformations of the data,
+#'   distances between points are generally less interpretable than a
+#'   linear transformation obtained by, say, PCA.
 #'
 #' @param fit An object of class \dQuote{poisson_nmf_fit} or
 #'   \dQuote{multinom_topic_model_fit}.
 #'
 #' @param dims The number of dimensions in the embedding. In
 #'   \code{tsne_from_topics}, this is passed as argument \dQuote{dims}
-#'   to \code{\link[Rtsne]{Rtsne}}.
+#'   to \code{\link[Rtsne]{Rtsne}}. In \code{umap_from_topics}, this is
+#'   passed as argument \dQuote{n_components} to
+#'   \code{\link[uwot]{umap}}.
 #'
 #' @param center A logical value indicating whether columns of
 #'   \code{fit$L} should be zero-centered before performing PCA; passed
@@ -27,7 +30,8 @@
 #'   \code{\link[stats]{prcomp}}.
 #' 
 #' @param \dots Additional arguments passed to
-#'   \code{\link[stats]{prcomp}} or \code{\link[Rtsne]{Rtsne}}.
+#'   \code{\link[stats]{prcomp}}, \code{\link[Rtsne]{Rtsne}} or
+#'   \code{\link[uwot]{umap}}.
 #'
 #' @return An n x d matrix containing the embedding, where n is the
 #'   number of rows of \code{fit$L}, and \code{d = dims}.
@@ -36,9 +40,6 @@
 #'   \code{\link{umap_plot}}, \code{\link[stats]{prcomp}},
 #'   \code{\link[Rtsne]{Rtsne}}, \code{\link[uwot]{umap}}
 #'
-#' @examples
-#' # Add examples here.
-#' 
 #' @importFrom stats prcomp
 #' 
 #' @export
@@ -55,8 +56,9 @@ pca_from_topics <- function (fit, dims = 2, center = TRUE,
 
 #' @rdname embeddings_from_topics
 #' 
-#' @param pca Whether to perform a PCA processing step in t-SNE;
-#'   passed as argument \dQuote{pca} to \code{\link[Rtsne]{Rtsne}}.
+#' @param pca Whether to perform a PCA processing step in t-SNE or
+#'   UMAP; passed as argument \dQuote{pca} to \code{\link[Rtsne]{Rtsne}}
+#'   or \code{\link[uwot]{umap}}.
 #'
 #' @param normalize Whether to normalize the data prior to running
 #'   t-SNE; passed as argument \dQuote{normalize} to
@@ -82,7 +84,7 @@ pca_from_topics <- function (fit, dims = 2, center = TRUE,
 #' 
 #' @param verbose If \code{verbose = TRUE}, progress updates are
 #'   printed; passed as argument \dQuote{verbose} to
-#'   \code{\link[Rtsne]{Rtsne}}.
+#'   \code{\link[Rtsne]{Rtsne}} or \code{\link[uwot]{umap}}.
 #' 
 #' @references
 #' Kobak, D. and Berens, P. (2019). The art of using t-SNE for
@@ -120,8 +122,6 @@ tsne_from_topics <- function (fit, dims = 2, pca = FALSE, normalize = FALSE,
   out <- Rtsne(fit$L,dims,pca = pca,normalize = normalize,eta = eta,
                perplexity = perplexity,theta = theta,max_iter = max_iter,
                check_duplicates = check_duplicates,verbose = verbose,...)
-
-  # Return the t-SNE embedding.
   Y <- out$Y
   rownames(Y) <- rownames(fit$L)
   colnames(Y) <- paste0("d",1:dims)
@@ -130,10 +130,47 @@ tsne_from_topics <- function (fit, dims = 2, pca = FALSE, normalize = FALSE,
 
 #' @rdname embeddings_from_topics
 #'
+#' @param n_neighbors Number of nearest neighbours in manifold
+#'   approximation; passed as argument \dQuote{n_neighbors} to
+#'   \code{\link[uwot]{umap}}.
+#'
+#' @param metric Distance matrix used to find nearest neighbors;
+#'   passed as argument \dQuote{metric} to
+#'   \code{\link[uwot]{umap}}.
+#'
+#' @param scale Scaling to apply to \code{fit$L}; passed as argument
+#'   \dQuote{scale} to \code{\link[uwot]{umap}}.
+#'
 #' @importFrom uwot umap
 #' 
 #' @export
 #' 
-umap_from_topics <- function (fit, dims, ...) {
+umap_from_topics <- function (fit, dims = 2, n_neighbors = 30,
+                              metric = "euclidean", scale = "none",
+                              pca = NULL, verbose = TRUE, ...) {
 
+  # Check input argument "fit".
+  if (!(inherits(fit,"poisson_nmf_fit") |
+        inherits(fit,"multinom_topic_model_fit")))
+    stop("Input \"fit\" should be an object of class \"poisson_nmf_fit\" or ",
+         "\"multinom_topic_model_fit\"")
+  n <- nrow(fit$L)
+  if (n < 4)
+    stop("umap cannot run on a matrix with fewer than 4 rows")
+
+  # Adjust n_neighbors if it is too large for the number of samples.
+  n0 <- n - 1
+  if (n_neighbors > n0) {
+    message(sprintf(paste("n_neighbors automatically changed to %d because",
+                          "the original setting of %d was too large for the",
+                          "number of samples (%d)"),n0,n_neighbors,n))
+    n_neighbors <- n0
+  }
+  
+  # Compute the UMAP embedding.
+  Y <- umap(fit$L,n_neighbors = n_neighbors,n_components = dims,pca = pca,
+            metric = metric,scale = scale,verbose = verbose,...)
+  rownames(Y) <- rownames(fit$L)
+  colnames(Y) <- paste0("d",1:dims)
+  return(Y)
 }
