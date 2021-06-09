4f44c5084b0dd1d030479310aabc98cbedd8a460
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-06-09 09:58:22 -0500

    Added examples illustrating pca_plot, tsne_plot and umap_plot.

--- R/fit_poisson_nmf.R
+++ R/fit_poisson_nmf.R
@@ -420,7 +420,7 @@ fit_poisson_nmf <- function (X, k, fit0, numiter = 100,
       method.text <- "CCD"
     cat(sprintf("Running %d %s updates, %s extrapolation ",numiter,
         method.text,ifelse(control$extrapolate,"with","without")))
-    cat("(fastTopics 0.5-39).\n")
+    cat("(fastTopics 0.5-40).\n")
   }
   
   # INITIALIZE ESTIMATES
