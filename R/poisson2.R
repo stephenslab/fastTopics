7be671fff8e46e414c61827e9a42587f40a47043
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-06-24 17:02:31 -0500

    A bit more code re-organization.

---
+++ R/poisson2.R
@@ -0,0 +1,94 @@
+
+# Return log-fold change statistics lfc[j,k], and accompanying
+# standard errors se[j,k] and z-scores z[j,k], under the Poisson glm
+# x[i,j] ~ Pois(u[i,j]), in which the Poisson rates are u[i,j] =
+# sum(L[i,]*F[j,]). The LFC calculations are determined by the "stat"
+# input argument: when stat = "vsmean", lfc[j,k] = log2(F[j,k]/mu[j]);
+# when stat = "le", lfc[j,k] is defined as the "least extreme"
+# pairwise log-fold change; and when stat = k', lfc[j,k] is the
+# pairwise log-fold change statistic lfc[j,k] = log2(F[j,k]/F[j,k']).
+compute_lfc_stats <- function (X, F, L, mu, stat = "vsmean",
+                               version = c("Rcpp","R")) {
+  if (version == "Rcpp") {
+    # TO DO.
+  } else {
+    m   <- ncol(X)
+    k   <- ncol(L)
+    lfc <- matrix(0,m,k)
+    se  <- matrix(0,m,k)
+    z   <- matrix(0,m,k)
+    for (i in 1:m) {
+      out     <- compute_lfc_stats_helper(X[,i],L,F[i,],mu[i],stat)
+      lfc[i,] <- out$lfc
+      se[i,]  <- out$se
+      z[i,]   <- out$z
+    }
+  }
+  return(list(lfc = lfc,se = se,z = z))
+}
+
+# Return log-fold change statistics lfc[j], and their standard errors
+# se[j] and z-scores z[j], under the Poisson glm x[i] ~ Pois(u[i]), in
+# which the Poisson rates are u[i] = sum(L[i,]*f). The LFC
+# calculations are determined by the "stat" input argument: when stat
+# = "vsmean", lfc[j] = log2(f[j]/mu); when stat = "le", lfc[j] is
+# defined as the "least extreme" pairwise log-fold change; and when
+# stat = k, lfc[j] is the pairwise log-fold change statistic lfc[j] =
+# log2(f[j]/f[k]).
+compute_lfc_stats_helper <- function (x, L, f, mu, stat) {
+  S <- compute_poisson_covariance(x,L,f)
+  if (stat == "vsmean")
+    out <- compute_lfc_vs_mean(f,mu,S)
+  else if (stat == "le")
+    out <- compute_lfc_le(f,S)
+  else
+    out <- compute_lfc_pairwise(f,S,stat)
+  lfc <- out$lfc
+  se  <- out$se
+  z   <- lfc/se
+  z[lfc == 0] <- 0
+  return(list(lfc = lfc/log(2),se = se/log(2),z = z))
+}
+
+# Return the log-fold change statistics lfc[i] = log(f[i]/mu), and
+# their standard errors se[i], under the Poisson glm with Poisson rates
+# L*f, given the MLE estimates (f), the mean or a comparable statistic
+# such as the median (mu), and S, the covariance of log(f). Here, the
+# log-fold change is defined using the natural logarithm (not the
+# base-2 logarithm, which is the convention).
+compute_lfc_vs_mean <- function (f, mu, S)
+  return(list(lfc = log(f/mu),se = sqrt(diag(S))))
+
+# Return the pairwise log-fold change statistics lfc[j] =
+# log(f[j]/f[i]), and their standard errors se[j], under the Poisson
+# glm with Poisson rates L*f, given the MLE estimates (f), and S, the
+# covariance of log(f). Here, the log-fold change is defined using the
+# natural logarithm (not the base-2 logarithm, which is the
+# convention).
+compute_lfc_pairwise <- function (f, S, i) {
+  lfc    <- log(f/f[i])
+  se     <- sqrt(diag(S) + S[i,i] - 2*S[i,])
+  lfc[i] <- 0
+  se[i]  <- 0
+  return(list(lfc = lfc,se = se))
+}
+
+# Return the "least extreme" pairwise log-fold change statistics
+# lfc[j] = log(f[j]/f[i]), and their standard errors, se[j], under the
+# Poisson glm with Poisson rates L*f. By "least extreme", we mean that
+# i != j is chosen so that the log-fold change is closest to
+# zero. Here, the log-fold change is defined using the natural
+# logarithm (not the base-2 logarithm, which is the convention). The
+# inputs are: f, the MLE estimates; and S, the covariance of log(f).
+compute_lfc_le <- function (f, S) {
+  n   <- length(f)
+  lfc <- rep(0,n)
+  se  <- rep(0,n)
+  for (i in 1:n) {
+    out    <- compute_lfc_pairwise(f,S,i)
+    j      <- order(abs(out$lfc))[2]
+    lfc[i] <- -out$lfc[j]
+    se[i]  <- out$se[j]
+  }
+  return(list(lfc = lfc,se = se))
+}
