1fdd4f9df4572ed8ce2c9cd01914b9522b0c2edc
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-03-18 16:19:11 -0500

    Filled out some roxygen2 docs for run_homer.

--- R/homer.R
+++ R/homer.R
@@ -1,29 +1,56 @@
-#' @title Perform HOMER Motif Enrichment Analysis Using DE Positions
+#' @title Perform HOMER Motif Enrichment Analysis using DE Genomic Positions
 #'
-#' @description http://homer.ucsd.edu/homer/
-#' See http://homer.ucsd.edu/homer/ngs/peakMotifs.html for more details.
+#' @description Run HOMER motif finding algorithm
+#'   (\code{findMotifsGenome.pl}) to identify motifs enriched for
+#'   differentially expressed (DE) genomic positions. See
+#'   \url{http://homer.ucsd.edu} for more information.
+#'
+#' @param de An object of class \dQuote{topic_model_de_analysis},
+#'   usually the result of running \code{\link{de_analysis}}.
+#'
+#' @param k Use the DE analysis results for this topic.
+#'
+#' @param positions A table of genomic positions corresponding to rows
+#'   of the \code{de_analysis} results. Specifically, it should a data
+#'   frame with four columns: \dQuote{chr}, chromosome name or number;
+#'   \dQuote{start}, start position of genomic feature; \dQuote{end},
+#'   end position of genomic feature; and \dQuote{name}, the name of the
+#'   genomic feature. If not specified, the genomic positions will be
+#'   extracted from the row names of \code{de$postmean}, in which the
+#'   row names are expected to be of the form \code{chr_start_end}. The
+#'   genomic positions will be written to a BED file (see
+#'   \url{https://genome.ucsc.edu/FAQ/FAQformat.html} for more
+#'   information about BED files).
+#'  
+#' @param genome The genome parameter passed to
+#'   \code{findMotifsGenome.pl}.
 #' 
-#' @param regions.file BED file of the peak regions.
-#' @param genome Genome assembly; e.g., "hg19"
-#' @param homer.path Directory path to 'findMotifsGenome.pl' excutable file.
-#' @param out.dir Output directory.
+#' @param subset Describe input argument "subset" here.
+#'
+#' @param homer.exec The name or file path of the HOMER
+#'   \code{findMotifsGenome.pl} excutable.
 #' 
-#' @param region.size Size of the regions used for motif finding (default=200)
-#' @param motif.length Length of motifs to be found (default=8,10,12).
-#' HOMER will find motifs of each size separately and then combine the results at the end.
-#' The length of time it takes to find motifs increases greatly with increasing size.
-#' @param optimize.count Number of mismatches allowed in global optimization phase (default=2)
-#' @param n.motifs Number of motifs to find (default=25)
-#' @param use.hypergeometric Whether to use hypergeometric distribution to score motif enrichment
-#' By default, findMotifsGenome.pl uses the binomial distribution to score motifs, which is faster.
-#' This works well when the number of background sequences greatly out number the target sequences.
-#' If the number of background sequences is smaller than target sequences, it is recommended to use the hypergeometric distribution.
-#' @param background BED file of the background regions
+#' @param out.dir The positions BED file and HOMER results are written
+#'   to this directory.
+#'
+#' @param homer.options Character string used to override default
+#'   \code{findMotifsGenome.pl} options.
+#'
+#' @param verbose When \code{verbose = TRUE}, progress information is
+#'   printed to the console.
 #' 
-#' @return A data frame of motif enrichment results loaed from HOMER's output 'knownResults.txt'
+#' @return A data frame of motif enrichment results loaed from HOMER's
+#'   output 'knownResults.txt'
 #'
 #' @importFrom utils read.table
 #' @importFrom utils write.table
+#' 
+#' @references
+#' Heinz, S., Benner, C., Spann, N., Bertolino, E., Lin, Y. C., Laslo,
+#' P., Cheng, J. X., Murre, C., Singh, H. and Glass, C. K. (2010).
+#' Simple combinations of lineage-determining transcription factors
+#' prime cis-regulatory elements required for macrophage and B cell
+#' identities. \emph{Molecular Cell} \bold{38}, 576-589.
 #' 
 #' @export
 #' 
@@ -64,13 +91,13 @@ run_homer <-
   homer.dir <- file.path(out.dir,"homer")
   homer.command <- paste(homer.exec,pos.file,genome,homer.dir,homer.options)
   if (verbose) {
-    cat("Performing HOMER motif enrichment analysis.\n")
+    cat("Performing HOMER motif enrichment analysis:\n")
     cat(homer.command,"\n")
   }
   system.out <- system(homer.command,intern = TRUE)
   homer.out <- read.table(file.path(homer.dir,"knownResults.txt"),
                           sep = "\t",header = TRUE,check.names = FALSE,
                           stringsAsFactors = FALSE)
-  return(res)
+  return(list(system.out = system.out,homer.out = homer.out))
 }
 
