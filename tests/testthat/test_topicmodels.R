79986441141f4eb0f7a463e0e887f951a87995f1
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-04-14 15:39:38 -0500

    Added test for topicmodels:::cost.

---
+++ tests/testthat/test_topicmodels.R
@@ -0,0 +1,15 @@
+context("topicmodels")
+
+test_that("R and C++ versions oftopicmodels::cost gives same result",{
+  set.seed(1)
+  n <- 8
+  m <- 12
+  k <- 4
+  X <- matrix(rpois(n*m,4),n,m)
+  Y <- as(X,"dgCMatrix")
+  A <- matrix(abs(rnorm(n*k)),n,k)
+  B <- matrix(abs(rnorm(k*m)),k,m)
+  y1 <- topicmodels:::cost(X,A,B,version = "r")
+  y2 <- topicmodels:::cost(Y,A,B,version = "cpp")
+  expect_equal(y1,y2,scale = 1,tolerance = 1e-10)
+})
