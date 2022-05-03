e7a121a5316cd87b78ee120d42fc9bb98a3ca420
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-05-03 16:22:19 -0500

    Made a few tweaks to check_map.R.

--- inst/code/check_map.R
+++ inst/code/check_map.R
@@ -1,4 +1,6 @@
 library(Compositional)
+
+# Simulate a 80 x 100 data set.
 set.seed(1)
 n <- 80
 m <- 100
@@ -7,7 +9,7 @@ dat <- simulate_count_data(n,m,k)
 X <- dat$X
 L <- dat$L
 F <- dat$F
-A <- matrix(abs(rnorm(m*k)) + 1,m,k)
+a <- matrix(abs(rnorm(m*k)) + 1,m,k)
 b <- abs(rnorm(k))
 
 N <- 100
@@ -16,33 +18,38 @@ f1 <- rep(0,N)
 f2 <- rep(0,N)
 f3 <- rep(0,N)
 for (i in 1:N) {
+  
+  # Compute the penalized likelihood for the multinomial topic model
+  # with a Dirichlet prior.
   fit <- list(L = L,F = F)
   class(fit) <- c("poisson_nmf_fit","list")
-  
-  # Compute multinomial topic model likelihood + Dirichlet prior.
   f0[i]  <- sum(loglik_multinom_topic_model(X,poisson2multinom(fit),e = 0))
   for (j in 1:k)
-    f0[i] <- f0[i] + ddiri(fit$F[,j],A[,j],logged = TRUE)
-
-  # Compute multinomial topic model likelihood with "pseudodata".
-  Y      <- rbind(X,t(A - 1))
+    f0[i] <- f0[i] + ddiri(fit$F[,j],a[,j],logged = TRUE)
+  
+  # Compute the multinomial topic model likelihood with "pseudodata".
+  Y      <- rbind(X,t(a - 1))
   fit2   <- fit
-  fit2$L <- rbind(fit$L,diag(b/(colSums(A - 1))))
+  fit2$L <- rbind(fit$L,diag(b/(colSums(a - 1))))
   f1[i]  <- sum(loglik_multinom_topic_model(Y,poisson2multinom(fit2),e = 0))
 
-  # Compute Poisson NMF likelihood with "pseudodata" (here we have to
-  # remove the "size factor" likelihoods).
-  f2[i]  <- sum(loglik_poisson_nmf(Y,fit2,e = 0))
-
-  # Compute the Poisson NMF likelihood + gamma prior.
-  f3[i]  <- sum(loglik_poisson_nmf(X,fit,e = 0))
+  # Compute the penalized Poisson NMF likelihood with a gamma prior.
+  f2[i] <- sum(loglik_poisson_nmf(X,fit,e = 0))
   for (j in 1:k)
-    f3[i] <- f3[i] + sum(dgamma(fit$F[,j],A[,j],b[j],log = TRUE))
+    f2[i] <- f2[i] + sum(dgamma(fit$F[,j],a[,j],b[j],log = TRUE))
   
+  # Compute Poisson NMF likelihood with "pseudodata".
+  f3[i] <- sum(loglik_poisson_nmf(Y,fit2,e = 0))
+
   # Tweak the fit.
   L <- L * matrix(exp(rnorm(n*k,sd = 0.1)),n,k)
   F <- F * matrix(exp(rnorm(m*k,sd = 0.1)),m,k)
 }
 
+# The multinomial penalized log-likelihoods and the multinomial
+# log-likelihoods with pseudodata should be equal up to a constant.
 plot(f0,f1,pch = 20)
+
+# The Poisson NMF penalized log-likelihoods and the Poisson NMF
+# log-likleihoods with pseudodata should be equal up to a constant.
 plot(f2,f3,pch = 20)
