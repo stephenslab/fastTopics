a8af175ef2878842ce22710ea8e9731bc735588c
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-05-04 10:19:04 -0500

    Added step to test_binom_fit.R script to fit Poisson NMF to binary data.

--- inst/code/test_binom_fit.R
+++ inst/code/test_binom_fit.R
@@ -15,10 +15,18 @@ L <- normalize.rows(L)
 F <- cbind(c(rep(0.08,m),rep(0.02,m),rep(0.05,m),rep(0.01,m)),
            c(rep(0.01,m),rep(0.01,m),rep(0.05,m),rep(0.008,m)),
            c(rep(0.08,m),rep(0.00,m),rep(0.05,m),rep(0.1,m)))
-P   <- L %*% t(F)
-n   <- nrow(P)
-m   <- ncol(P)
-X   <- matrix(rbinom(n*m,1,P),n,m)
+P <- L %*% t(F)
+n <- nrow(P)
+m <- ncol(P)
+X <- matrix(rbinom(n*m,1,P),n,m)
+print(unique(as.vector(X)))
+# TO DO: Try also the case when M is sparse.
+# X <- as(X,"dgCMatrix")
 sim <- list(L = L,F = F,X = X)
 
-# TO DO: Try also the case when M is sparse.
+# Fit a Poisson non-negative matrix factorization to the binomial
+# data.  To simplify comparison with the "true" factorization---that
+# is, the L and F used to simulate the data--the factorization is
+# initialized to the true parameter values.
+res <- init_poisson_nmf(X,L = L,F = F)
+res <- fit_poisson_nmf(X,fit0 = res,control = list(extrapolate = TRUE))
